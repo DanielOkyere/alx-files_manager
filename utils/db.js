5b150b0ce9ee0e524531b839be0ed1ff9be3ebbb
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-04-03 00:59:01 +0000

    fixed linting issues with db.js

--- utils/db.js
+++ utils/db.js
@@ -3,9 +3,6 @@
  * Contains class DBClient
  */
 import { MongoClient } from 'mongodb';
-//import * as dotenv from 'dotenv';
-
-//dotenv.config()
 
 class DBClient {
   constructor() {
@@ -13,12 +10,12 @@ class DBClient {
     this.PORT = process.env.DB_PORT || '27017';
     this.DB = process.env.DB_DATABASE || 'files_manager';
     this.url = `mongodb://${this.HOST}:${this.PORT}/${this.DB}`;
-    this.client = new MongoClient(this.url, {useUnifiedTopology: true});
+    this.client = new MongoClient(this.url, { useUnifiedTopology: true });
     this.client.connect();
   }
 
   isAlive() {
-    return this.client.isConnected();;
+    return this.client.isConnected();
   }
 
   async nbUsers() {
